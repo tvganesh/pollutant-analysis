7bcbe4aa66c39870f1d3b9a82f24a0d5f06f0c5e
Tinniam V Ganesh <tvganesh.85@gmail.com>
2015-03-17 10:07:52 +0530

    plot5.R corrections

--- plot5.R
+++ plot5.R
@@ -3,24 +3,30 @@ library(dplyr)
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-# Get all motor vehicles - based on category "on road"
+#Subset the PM2.5 emissions for Baltimore city fips = 24510
+a <- NEI$fips == "24510"
+baltimore <- NEI[a,]
 
+
+# Get all motor vehicles - based on category "on road"
 a <- SCC$Data.Category == "Onroad"
 scc <- SCC[a,1]
 
-# Filter all rows with the given SCC values
-vehicles <- NULL
+#Initialize to NULL
+bvehicles <- NULL
+
+# Filter all rows in Baltimore dataframe for on road emissions
 for(i in 1:length(scc)) {
-  a<- filter(la,SCC==scc[i])
-  vehicles <- rbind(vehicles,a)
+  a<- filter(baltimore,SCC==scc[i])
+  bvehicles <- rbind(bvehicles,a)
 }
 
 # Compute the Emissions  using the dplyr summarise
-emissions <- vehicles %>% group_by(year)  %>% summarise(m = sum(Emissions))
+emissions <- bvehicles %>% group_by(year)  %>% summarise(m = sum(Emissions))
 
 # Save plot
 png("plot5.png")
 plot(emissions$year,emissions$m, xlab="Year",xlim=c(1998,2009),
-     ylab='Total Emissions',main="Total PM2.5 motor vehicle emissions vs year")
+     ylab='Total Emissions',main="Total Baltimore motor vehicle emissions vs year")
 lines(emissions$year,emissions$m,col="blue",lwd=3.0)
 dev.off()
